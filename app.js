e1318b59d93ebfae5efe32c86fc327ba46cd0d82
andreajiao <andreajiao@gmail.com>
2022-10-03 15:11:32 +0200

    first push

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+
+import path from "path"
+
+app.use(express.static("public"));
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve("./public/frontpage.html"));
+})
+
+const PORT = process.env.PORT || 8080;
+
+const server =app.listen(PORT,(error)=>
+{
+if(error){
+    console.log(error);
+}
+console.log("Server is running on port", server.address().port)
+});
